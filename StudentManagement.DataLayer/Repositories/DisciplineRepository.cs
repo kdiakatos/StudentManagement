823bc0e7bde6df3f2bb881eab2b247662f58558e
Konstantinos Diakatos <kdiakatos@hotmail.gr>
2021-02-05 22:36:21 +0200

    Discipline Repository Minor Change

--- StudentManagement.DataLayer/Repositories/DisciplineRepository.cs
+++ StudentManagement.DataLayer/Repositories/DisciplineRepository.cs
@@ -29,6 +29,7 @@ namespace StudentManagement.DataLayer.Repositories
             {
                 dbRecord.DisciplineName = discipline.DisciplineName;
                 dbRecord.ProfesorName = discipline.ProfesorName;
+                dbRecord.Score = discipline.Score;
                 await _smContext.SaveChangesAsync();
             }
             return discipline;
