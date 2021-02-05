73cc8e73a859d1a2d19bc8f08601c57105965eb6
Konstantinos Diakatos <kdiakatos@hotmail.gr>
2021-02-05 22:31:44 +0200

    Add Semester Respository/Interface

---
+++ StudentManagement.DataLayer/Interfaces/ISemesterRepository.cs
@@ -0,0 +1,14 @@
+﻿using StudentManagement.DataLayer.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace StudentManagement.DataLayer.Interfaces
+{
+    public interface ISemesterRepository
+    {
+        Task<Semester> CreateSemesterAsynd(Semester semester);
+        Task DeleteSemesterAsync(Guid id);
+        Task<Semester> GetSemesterByIdAsync(Guid id);
+        Task<Semester> UpdateSemesterAsync(Semester semester);
+    }
+}
--- StudentManagement.DataLayer/Repositories/SemesterRepository.cs
+++ StudentManagement.DataLayer/Repositories/SemesterRepository.cs
@@ -1,10 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.DataLayer.Entities;
+using StudentManagement.DataLayer.Interfaces;
+using System;
+using System.Threading.Tasks;
 
 namespace StudentManagement.DataLayer.Repositories
 {
-    public class SemesterRepository
+    public class SemesterRepository : ISemesterRepository
     {
+        private readonly SMContext _smContext;
+
+        public SemesterRepository(SMContext smContext)
+        {
+            _smContext = smContext;
+        }
+
+        public async Task<Semester> CreateSemesterAsynd(Semester semester)
+        {
+            await _smContext.AddAsync(semester);
+            await _smContext.SaveChangesAsync();
+            return semester;
+        }
+
+        public async Task<Semester> UpdateSemesterAsync(Semester semester)
+        {
+            var dbRecord = await _smContext.Semesters.FirstOrDefaultAsync(x => x.SemesterId == semester.SemesterId);
+            if (dbRecord != null)
+            {
+                dbRecord.Name = semester.Name;
+                dbRecord.StartDate = semester.StartDate;
+                dbRecord.EndDate = semester.EndDate;
+                await _smContext.SaveChangesAsync();
+            }
+            return semester;
+        }
+
+        public async Task<Semester> GetSemesterByIdAsync(Guid id)
+        {
+            return await _smContext.Semesters.FirstOrDefaultAsync(x => x.SemesterId == id);
+        }
+
+        public async Task DeleteSemesterAsync(Guid id)
+        {
+            var dbRecord = await _smContext.Semesters.FirstOrDefaultAsync(x => x.SemesterId == id);
+            _smContext.Remove(dbRecord);
+            await _smContext.SaveChangesAsync();
+        }
     }
 }
