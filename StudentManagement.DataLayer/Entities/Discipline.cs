268a8933c17487b159f42f00ac1a61256e0ce2dd
Konstantinos Diakatos <kdiakatos@hotmail.gr>
2021-02-05 22:34:33 +0200

    Discipline Table Minor Change

--- StudentManagement.DataLayer/Entities/Discipline.cs
+++ StudentManagement.DataLayer/Entities/Discipline.cs
@@ -9,6 +9,7 @@ namespace StudentManagement.DataLayer.Entities
         public Guid DisciplineId { get; set; }
         public string DisciplineName { get; set; }
         public string ProfesorName { get; set; }
+        public int Score { get; set; }
         public ICollection<SemesterDiscipline> SemesterDisciplines { get; set; }
     }
 }
---
+++ StudentManagement.DataLayer/Migrations/20210205203342_DisciplineTableMinorChange.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentManagement.DataLayer.Migrations
+{
+    public partial class DisciplineTableMinorChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "Disciplines",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "Disciplines");
+        }
+    }
+}
