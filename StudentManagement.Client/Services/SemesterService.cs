b1d8de2a177fe4a5fa571573969144e0d348d7ee
Konstantinos Diakatos <kdiakatos@hotmail.gr>
2021-02-07 15:42:19 +0200

    Add Client Services

---
+++ StudentManagement.Client/Services/SemesterService.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using StudentManagement.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Client.Services
+{
+    public class SemesterService
+    {
+        public HttpClient Client { get; }
+
+        private readonly IConfiguration _configuration;
+
+        public SemesterService(HttpClient client, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var baseUrl = configuration.GetValue<string>("HttpClientSettings:BaseURL");
+            client.BaseAddress = new Uri(baseUrl);
+            Client = client;
+        }
+
+        public async Task<List<SemesterViewModel>> GetSemestersListAsync()
+        {
+            var semestersList = new List<SemesterViewModel>();
+            try
+            {
+
+                var response = await Client.GetAsync("api/semester");
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                    var result = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<SemesterViewModel>>(jsonResponse, options);
+                    semestersList = result.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return semestersList;
+        }
+
+        public async Task<SemesterViewModel> GetSemesterByIdAsync(Guid semesterId)
+        {
+            var semester = new SemesterViewModel();
+            try
+            {
+                var response = await Client.GetAsync("api/semester/" + semesterId);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                    semester = System.Text.Json.JsonSerializer.Deserialize<SemesterViewModel>(jsonResponse, options);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return semester;
+        }
+
+        public async Task<SemesterViewModel> AddSemesterAsync(SemesterViewModel semester)
+        {
+            try
+            {
+                var jsonPayload = JsonConvert.SerializeObject(semester);
+                var body = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+                var response = await Client.PostAsync("api/semester", body);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return semester;
+        }
+
+        public async Task<SemesterViewModel> UpdateSemesterAsync(SemesterViewModel semester)
+        {
+            try
+            {
+                var jsonPayload = JsonConvert.SerializeObject(semester);
+                var body = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+                var response = await Client.PutAsync("api/semester", body);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return semester;
+        }
+    }
+}
---
+++ StudentManagement.Client/Services/StudentService.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using StudentManagement.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Client.Services
+{
+    public class StudentService
+    {
+        public HttpClient Client { get; }
+
+        private readonly IConfiguration _configuration;
+
+        public StudentService(HttpClient client, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var baseUrl = configuration.GetValue<string>("HttpClientSettings:BaseURL");
+            client.BaseAddress = new Uri(baseUrl);
+            Client = client;
+        }
+
+        public async Task<List<StudentViewModel>> GetStudentListAsync()
+        {
+            var studentsList = new List<StudentViewModel>();
+            try
+            {
+
+                var response = await Client.GetAsync("api/student");
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                    var result = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<StudentViewModel>>(jsonResponse, options);
+                    studentsList = result.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return studentsList;
+        }
+
+        public async Task<StudentViewModel> GetStudentByIdAsync(Guid studentId)
+        {
+            var student = new StudentViewModel();
+            try
+            {
+                var response = await Client.GetAsync("api/student/" + studentId);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+                    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                    student = System.Text.Json.JsonSerializer.Deserialize<StudentViewModel>(jsonResponse, options);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return student;
+        }
+
+        public async Task<StudentViewModel> AddStudentAsync(StudentViewModel student)
+        {
+            try
+            {
+                var jsonPayload = JsonConvert.SerializeObject(student);
+                var body = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+                var response = await Client.PostAsync("api/student", body);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return student;
+        }
+
+        public async Task<StudentViewModel> UpdateStudentAsync(StudentViewModel student)
+        {
+            try
+            {
+                var jsonPayload = JsonConvert.SerializeObject(student);
+                var body = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+                var response = await Client.PutAsync("api/student", body);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return student;
+        }
+    }
+}
